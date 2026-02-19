5c9906dcba032ba394619d5d23ca222e352bbbc1
ApexUmrao <74846759+ApexUmrao@users.noreply.github.com>
2026-02-19 17:03:43 +0530

    Autowiring Spring Concept

---
+++ SpringAutowire/src/com/apex/beans/Student.java
@@ -0,0 +1,44 @@
+package com.apex.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Student {
+	
+	private String name;
+	private int rollNo;
+	
+	@Autowired
+	@Qualifier("Addr1")  // @Qualifier("createAddrObj2()")
+	private Address address;
+	
+	@Autowired
+	private Subject subject;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", rollNo=" + rollNo + ", address=" + address + ", subject=" + subject + "]";
+	}
+	
+	public void show() {
+		System.out.println("Name : " + name);
+		System.out.println("Roll No : " + rollNo);
+		System.out.println("Address : " + address);
+		System.out.println("Subject : " + subject);
+
+
+	}
+
+}
---
+++ SpringAutowire/src/resources/JavaConfigAW.java
@@ -0,0 +1,65 @@
+package resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.apex.beans.Address;
+import com.apex.beans.Student;
+import com.apex.beans.Subject;
+
+@Configuration
+public class JavaConfigAW {
+	
+	@Bean
+	public Address createAddrObj() {
+		Address addr = new Address();
+		addr.setHouseNo(456);
+		addr.setCity("Noida");
+		addr.setState("UP");
+		return addr;
+	}
+	
+	@Bean("Addr1")
+	public Address createAddrObj2() {
+		Address addr = new Address();
+		addr.setHouseNo(353);
+		addr.setCity("Delhi");
+		addr.setState("Delhi");
+		return addr;
+	}
+	
+	@Bean
+	public Subject createSubObj() {
+		Subject sub = new Subject();
+		
+		List<String> listSub = new ArrayList<String>();
+		listSub.add("Java");
+		listSub.add("Python");
+		listSub.add("DSA");
+		
+		sub.setSub(listSub);
+		
+		return sub;
+	}
+	
+	@Bean("stud")
+	public Student createStdObj() {
+		Student std = new Student();
+		
+		std.setRollNo(1);
+		std.setName("Aryan");
+		
+		
+		//Manual Deopendcy Injection -- NO Autowiring
+		//std.setAddress(createAddrObj());
+		
+		//std.setSubject(createSubObj());
+		
+		
+		return std;
+	}
+
+}
