92e544270345fbdb73df562f1b38d856f3af5ad9
ApexUmrao <umraoshivanshu@gmail.com>
2026-02-19 23:04:43 +0530

    Autowiring Spring Concept Modified

--- SpringAutowire/src/com/apex/main/MainAppAutowire.java
+++ SpringAutowire/src/com/apex/main/MainAppAutowire.java
@@ -4,25 +4,25 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.apex.beans.Student;
+import com.apex.beans.StudentAutowire;
 
 import resources.JavaConfigAW;
 
-public class MainApp {
+public class MainAppAutowire {
 	
 	public static void main(String [] args) {
 
 		System.out.println("AutoWiring With XML Config");
 		String path = "/resources/XMLConfig.xml";
 		ApplicationContext ac = new ClassPathXmlApplicationContext(path);
-		Student s1 = (Student) ac.getBean("studentObj");
+		StudentAutowire s1 = (StudentAutowire) ac.getBean("studentObj");
 		s1.show();
 		
 		System.out.println("-----------------------------------------");
 
 		System.out.println("AutoWiring With Java Config");
 		ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfigAW.class);
-		Student s2 = (Student) context.getBean("stud");
+		StudentAutowire s2 = (StudentAutowire) context.getBean("stud");
 		s2.show();
 	}
 
