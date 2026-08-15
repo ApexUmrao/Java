51d3955488859acbe34757c81651cf91fef95d9c
ApexUmrao <umraoshivanshu@gmail.com>
2026-08-15 13:56:54 +0530

    Cognizant Code

--- Concept/src/InterviewCoding/Congizant.java
+++ Concept/src/InterviewCoding/Congizant.java
@@ -1,5 +1,9 @@
 package InterviewCoding;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Congizant {
@@ -7,12 +11,31 @@ public class Congizant {
     public static void main (String[] args) {
 
         String str = "abracadabra";
-            String result = str.chars()
+             str.chars()
                     .distinct()
                     .mapToObj(c -> String.valueOf((char) c))
-                    .collect(Collectors.joining());
+                    .forEach(System.out::print);
+                   // .collect(Collectors.joining());
 
-            System.out.println(result);
+          //  System.out.println(result);
+             
+             
+          List<Integer> numbers = Arrays.asList(0,1, 2, 3, 4, 5, 3, 4, 4,4,5, 6, 7, 8, 3,9, 10);
+          
+          numbers.stream()
+          		 .distinct()
+          		 .filter((n -> numbers.stream()
+          				 .filter(x -> x.equals(n))
+          				 .count() == n))
+          		 .forEach(System.out::println);
+          				 
+          numbers.stream()
+		  		 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+		  		 .entrySet()
+		  		 .stream()
+	                .filter(e -> e.getKey() != null && e.getValue() == e.getKey().longValue()) // avoid nulls
+		  		 .map(Map.Entry::getKey)
+		  		 .forEach(System.out::println);
         }
     }
 
