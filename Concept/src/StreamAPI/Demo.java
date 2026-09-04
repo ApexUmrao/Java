6bf88046e0e317bf02922497187ecacd01b834c9
ApexUmrao <umraoshivanshu@gmail.com>
2026-09-04 12:21:52 +0530

    New Question

--- Concept/src/StreamAPI/Demo.java
+++ Concept/src/StreamAPI/Demo.java
@@ -99,5 +99,21 @@ public class Demo {
                 .sum();
 		
 		System.out.println("Sum: " + sum);
+		
+		int[] arr = {0,1,0,0,1,0,1,1,0,1,0};
+         
+		int left = 0;
+		for (int i = 0; i<arr.length; i++) {
+			if (arr[i] == 1) {
+				arr[left] = arr[i];
+				arr[i] = 0;
+				left++;
+			}
+			
+		}
+		
+		for (int i = 0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 		}
 }
